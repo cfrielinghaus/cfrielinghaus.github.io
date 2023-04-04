837e95673ffa7aebace07b4aa816c9d6347b47c8
Christopher Frielinghaus <cfrielinghaus11@gmail.com>
2023-04-04 08:32:32 -0400

    fine, I'll do it myself

--- capstone_wizardry.js
+++ capstone_wizardry.js
@@ -1,15 +1,7 @@
 // Initialize the map
 function initMap() {
-    var location = {lat: 51.505, lng: -0.09};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
-      center: location
-    });
-  
-    // Add a marker to the map at a specific location
-    var marker = new google.maps.Marker({
-      position: location,
-      map: map
-    });
-  }
-  
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: {lat: 34.0522, lng: -118.2437}
+  });
+}
