6cef1680fa5071d924a470bce10d583e0d2c399a
Christopher Frielinghaus <cfrielinghaus11@gmail.com>
2023-06-01 10:07:05 -0400

    Final Changes: C-Moon. 2019-12-05 11:00

--- report.php
+++ report.php
@@ -1,22 +1,11 @@
 <?php
-// Get the form data
-$location = $_POST['location'];
-$incident = $_POST['incident'];
-$description = $_POST['description'];
-$time = $_POST['time'];
-
-// Sanitize the form data
-$location = filter_var($location, FILTER_SANITIZE_STRING);
-$incident = filter_var($incident, FILTER_SANITIZE_STRING);
-$description = filter_var($description, FILTER_SANITIZE_STRING);
-$time = filter_var($time, FILTER_SANITIZE_STRING);
-
-// Connect to the database
+// Database credentials
 $servername = "localhost";
 $username = "username";
 $password = "password";
-$dbname = "myDB";
+$dbname = "database_name";
 
+// Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
@@ -24,15 +13,20 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Insert the form data into the database
-$sql = "INSERT INTO incident_reports (location, incident, description, time)
-        VALUES ('$location', '$incident', '$description', '$time')";
+// Get the crime information from the form
+$crime_type = $_POST["crime-type"];
+$description = $_POST["description"];
+$time = $_POST["time"];
+$location = $_POST["location"];
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+// Prepare a statement to insert the crime information into the database
+$stmt = $conn->prepare("INSERT INTO crime_reports (crime_type, description, time, location) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $crime_type, $description, $time, $location);
+$stmt->execute();
 
+// Display a success message
+echo "Crime report submitted successfully.";
+
+// Close the connection
 $conn->close();
 ?>
